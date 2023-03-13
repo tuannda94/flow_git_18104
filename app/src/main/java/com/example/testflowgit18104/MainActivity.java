5f9c7486d80dab120eb11befd0cb7928734cde2f
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-03-13 13:03:12 +0700

    Tuannda3: Chức năng X

--- app/src/main/java/com/example/testflowgit18104/MainActivity.java
+++ app/src/main/java/com/example/testflowgit18104/MainActivity.java
@@ -11,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    Thực hiện chức năng của tuannda3
 }
